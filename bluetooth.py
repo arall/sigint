060b34ecd27d0622de72e9464db611750bb3024e
Gerard Arall <gerard.arall@gmail.com>
2020-01-10 15:24:10 +0100

    first commit

---
+++ bluetooth.py
@@ -0,0 +1,28 @@
+import subprocess
+import re
+
+devices = []
+
+subprocess.Popen('btmgmt le on', stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen('sudo btmgmt find', stdout=subprocess.PIPE, shell=True)
+output = proc.communicate()
+for line in str(output).split('\\n')[:-1]:
+    if 'hci0 dev_found' in line:
+        # Store the previous
+        if 'device' in locals():
+            devices.append(device);
+        device = {}
+        m = re.search('dev_found: (([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})) type (.*?) rssi (-\d+) flags', line)
+        device['mac'] = m.group(1)
+        device['type'] = m.group(4)
+        device['rssi'] = m.group(5)
+        device['time'] = int(time.time())
+    elif 'AD flags ' in line:
+        m = re.search('AD flags (.*)', line)
+        device['flags'] = m.group(1).rstrip()
+    elif 'name ' in line:
+        m = re.search('name (.*)', line)
+        device['name'] = m.group(1).rstrip()
+
+for device in devices:
+    print device
---
+++ wifi.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2.7
+# Based on :https://gist.github.com/LoranKloeze/6b713022619c2b32b32c6400a55a8433
+
+import subprocess
+import re
+import time
+
+monitor_dev = "wlan1mon"
+
+while True:
+    proc = subprocess.Popen(['tcpdump', '-l', '-I', '-i', monitor_dev, '-e', '-s', '256', 'type', 'mgt', 'subtype', 'probe-req'],stdout=subprocess.PIPE)
+    patt = '(-\d+)dBm signal antenna 0.+SA:([0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+) .+(Probe Request) \((.+)\)'
+    while True:
+        line = proc.stdout.readline()
+        if line != '':
+            m = re.search(patt, line)
+            if m is not None and len(m.groups()) == 4:
+                probe = {
+                    'signal': m.group(1).rstrip(),
+                    'mac': m.group(2).rstrip(),
+                    'ssid': m.group(4).rstrip(),
+                    'time': int(time.time()),
+                }
+                print probe
+        else:
+            break
