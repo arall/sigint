42f8e3e40ed8557f6ae0e3cd42d741b12cbfc41b
Gerard Arall <gerard.arall@gmail.com>
2020-05-03 21:29:51 +0200

    Fix user class

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -15,7 +15,7 @@ class User extends Resource
      *
      * @var string
      */
-    public static $model = \App\User::class;
+    public static $model = \App\Models\User::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
