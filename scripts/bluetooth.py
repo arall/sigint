85cee5a83c2cd6805a904a345131652d4b5fa9a6
Gerard Arall <gerard.arall@gmail.com>
2020-05-26 16:26:58 +0200

    BT loop

--- scripts/bluetooth.py
+++ scripts/bluetooth.py
@@ -9,33 +9,33 @@ load_dotenv()
 
 headers = {"Authorization": "Bearer " + os.getenv('API_KEY')}
 
-devices = []
-
 subprocess.Popen('hciconfig hci0 up', stdout=subprocess.PIPE, shell=True)
 subprocess.Popen('btmgmt le on', stdout=subprocess.PIPE, shell=True)
 
-proc = subprocess.Popen('sudo btmgmt find', stdout=subprocess.PIPE, shell=True)
-output = proc.communicate()
-for line in str(output).split('\\n')[:-1]:
-    if 'hci0 dev_found' in line:
-        # Store the previous
-        if 'device' in locals():
-            devices.append(device);
-        device = {}
-        m = re.search('dev_found: (([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})) type (.*?) rssi (-\d+) flags', line)
-        device['type_id'] = 1
-        device['identifier'] = m.group(1)
-        device['type'] = m.group(4)
-        device['signal'] = m.group(5)
-        device['time'] = int(time.time())
-    elif 'AD flags ' in line:
-        m = re.search('AD flags (.*)', line)
-        device['flags'] = m.group(1).rstrip()
-    elif 'name ' in line:
-        m = re.search('^name (.*)', line)
-        if m:
-            device['name'] = m.group(1).rstrip()
+while True:
+    devices = []
+    proc = subprocess.Popen('sudo btmgmt find', stdout=subprocess.PIPE, shell=True)
+    output = proc.communicate()
+    for line in str(output).split('\\n')[:-1]:
+        if 'hci0 dev_found' in line:
+            # Store the previous
+            if 'device' in locals():
+                devices.append(device);
+            device = {}
+            m = re.search('dev_found: (([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})) type (.*?) rssi (-\d+) flags', line)
+            device['type_id'] = 1
+            device['identifier'] = m.group(1)
+            device['type'] = m.group(4)
+            device['signal'] = m.group(5)
+            device['time'] = int(time.time())
+        elif 'AD flags ' in line:
+            m = re.search('AD flags (.*)', line)
+            device['flags'] = m.group(1).rstrip()
+        elif 'name ' in line:
+            m = re.search('^name (.*)', line)
+            if m:
+                device['name'] = m.group(1).rstrip()
 
-for device in devices:
-    print device
-    requests.post(os.getenv('API_URL') + 'logs', data=device, headers=headers)
+    for device in devices:
+        print device
+        requests.post(os.getenv('API_URL') + 'logs', data=device, headers=headers)
